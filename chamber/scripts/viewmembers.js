39692b454d3025cd526686c9123040fde7787332
Joseph-Marlow <jmarlow@byupathway.edu>
2025-09-16 16:06:58 -0700

    file pathing issues

--- chamber/scripts/viewmembers.js
+++ chamber/scripts/viewmembers.js
@@ -92,7 +92,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // fetch members.json
   async function loadMembers() {
     try {
-      const res = await fetch('./scripts/members.json', { cache: 'no-cache' });
+      const res = await fetch('./chamber/scripts/members.json', { cache: 'no-cache' });
       if (!res.ok) throw new Error(`HTTP ${res.status}`);
       const data = await res.json();
       members = data;
