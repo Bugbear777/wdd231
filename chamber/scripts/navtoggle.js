aeef9795b5a82d43ef5651848e8cf6a1c124e666
Joseph-Marlow <jmarlow@byupathway.edu>
2025-10-04 16:18:50 -0700

    hamburger works

---
+++ chamber/scripts/navtoggle.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navToggle = document.getElementById("navToggle");
+  const siteHeader = document.querySelector(".site-header");
+
+  navToggle.addEventListener("click", () => {
+    const isOpen = siteHeader.classList.toggle("open");
+    navToggle.setAttribute("aria-expanded", isOpen);
+  });
+});
+s
